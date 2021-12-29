4a741163a0625742f41870816f842b7e4d6ccfd8
PtruckStar <shamshab00@gmail.com>
2021-12-29 21:40:37 +0800

    THE DARK MODE IS ALIVE!!!

--- maid_skills/animasu_scraper/view/html.js
+++ maid_skills/animasu_scraper/view/html.js
@@ -1,10 +1,19 @@
 const h = (content, title) => { return `
-  <html>
+  <html color-mode="light">
   <head>
   <title>${title}</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script>
+    if (
+        localStorage.getItem('color-mode') === 'dark' ||
+        (window.matchMedia('(prefers-color-scheme: dark)').matches &&
+         !localStorage.getItem('color-mode'))
+    ) {
+        document.documentElement.setAttribute('color-mode', 'dark')
+    }
+  </script>
   <style>
-    :root {
+    :root[color-mode="dark"] {
       --background: #1D1D1D;
       --foreground: #0C0C0C;
       --lighten: #060606;
@@ -12,6 +21,25 @@ const h = (content, title) => { return `
       --shadow: #151515;
       --text: #d1d1d1;
       --subtext: gray;
+    }
+    :root[color-mode="light"] {
+      --background: #f1f1f1;
+      --foreground: #fff;
+      --lighten: #D0D0D0;
+      --blues: #3478F6;
+      --shadow: #E4E4E4;
+      --text: black;
+      --subtext: #444;
+    }
+    :root[color-mode="light"] .light--hidden {
+        display: none;
+    }
+    :root[color-mode="dark"] .dark--hidden {
+        display: none;
+    }
+    
+    * {
+      transition: all 0.5s ease-in;
     }
     body {
       background-color: var(--background);
@@ -35,7 +63,6 @@ const h = (content, title) => { return `
     a.list {
       text-decoration: none;
       font-family: sans-serif;
-      color: var(--text);
       overflow: hidden;
       padding-bottom: 0.5rem;
       background-color: var(--foreground);
@@ -49,21 +76,31 @@ const h = (content, title) => { return `
     a.list > h3 {
       margin: 0;
       padding: 0 0.5rem;
+      color: var(--text);
     }
     a.list > span {
       font-size: 0.75rem;
       padding: 0 0.5rem;
+      color: var(--subtext);
     }
     
+    .search_wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      width: 100%;
+      max-width: 600px;
+    }
     form {
       width: 100%;
       background-color: var(--foreground);
       border-radius: 0.5rem;
       padding: 0;
       margin: 1em 0;
+      margin-left: 0.5rem;
       overflow: hidden;
-      max-width: 600px;
-      }
+      flex-basis: auto;
+    }
     input:focus {
       border: none;
       outline: none;
@@ -220,6 +257,21 @@ const h = (content, title) => { return `
       border-radius: 0.5rem;
     }
     
+    .color-mode__btn{
+      border: solid 1px var(--blues);
+      border-radius: 0.5rem;
+      background-color: var(--foreground);
+      height: 2rem;
+      /*box-shadow: 
+        -2px -2px 5px -3px var(--blues),
+        2px 2px 5px -3px var(--blues);*/
+    }
+    .toggle_svg {
+      width: 1.5rem;
+      padding: 0 0.5rem;
+      color: var(--blues);
+    }
+    
     .shadow {
       box-shadow: 
         -5px -5px 5px -3px var(--shadow),
@@ -242,6 +294,22 @@ const h = (content, title) => { return `
   <div id="footer">
     <p>rahayu.</p>
   </div>
+  
+  <script>
+  const toggleColorMode = e => {
+    if (e.currentTarget.classList.contains("light--hidden")) {
+      	document.documentElement.setAttribute("color-mode", "light");
+  		localStorage.setItem("color-mode", "light")
+  		return;
+  	}      
+    document.documentElement.setAttribute("color-mode", "dark");
+  	localStorage.setItem("color-mode", "dark");
+  };
+  const toggleColorButtons = document.querySelectorAll(".color-mode__btn");
+  toggleColorButtons.forEach(btn => {
+      btn.addEventListener("click", toggleColorMode);
+  });
+  </script>
   
   </body>
   </html>`
